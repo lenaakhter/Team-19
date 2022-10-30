b2c9e0a7f57b7ff02f972131f275651b2549b4d1
Hasnain Ali <hasnain7815@gmail.com>
2022-10-30 17:26:20 +0000

    added routes and views for  products and contact page

--- Sarth-website/resources/views/welcome.blade.php
+++ Sarth-website/resources/views/welcome.blade.php
@@ -21,10 +21,11 @@
     <a href="/" class="logo">Sarth</a>
     <div class="header-right">
         <a class="active" href="/">Home</a>
-        <a href="/">Products</a>
-        <a href="/">Contact</a>
+        <a href="/products">Products</a>
+        <a href="/contact">Contact</a>
     </div>
-</div> 
+<div>
+
 
 @endsection
 
--- Sarth-website/routes/web.php
+++ Sarth-website/routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/products', function () {
+    return view('products');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
