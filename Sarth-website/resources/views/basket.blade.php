c2a0f20c943a94cf07e8ff87e4ae88c1709e4d50
Hasnain Ali <hasnain7815@gmail.com>
2022-11-10 19:58:17 +0000

    protected the route for basket if someone write /basket it will say you need to login to view that page

--- Sarth-website/resources/views/basket.blade.php
+++ Sarth-website/resources/views/basket.blade.php
@@ -7,16 +7,19 @@
     <div class="title">
     
         <h1>Your basket, {{Session::get('user')['name']}}</h1>
+
+        <h3> Item has been added to {{Session::get('user')['email']}}'s Basket</h3>
        
     </div>
 @else 
 <div>
     
-<h1> User not logged in</h1>
+<h1>You are not logged in</h1>
+
+<h3>Click here to <a href="/login">login</a></h3>
+
 </div>
 @endif
 </div>
 
-
-<h3> Item has been added to {{Session::get('user')['email']}}'s Basket</h3>
 @endsection
