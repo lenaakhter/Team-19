c71bae119268581e5d07a9d0d6b35fd472227376
Hasnain Ali <hasnain7815@gmail.com>
2022-11-10 16:16:09 +0000

    changes on the item.blade.php

--- Sarth-website/resources/views/item.blade.php
+++ Sarth-website/resources/views/item.blade.php
@@ -9,7 +9,7 @@
     <img src="{{ $item->imageLocation }} " alt="game" height="500px" width="400px"> 
     <div>
         <p>
-            <h2>Game Descripton</h2>
+        <h2>Game Descripton</h2>
             {{ $item->productDescription }} <br><br>
             <h2>Age Rating</h2>
             {{ $item->ageRating }} <br><br>
@@ -17,9 +17,10 @@
             £{{ $item->price }} <br><br>
             <form action="/basket" method = "post">
                 @csrf
-            <input type="hidden" name = "productID" value = "{{$item->productID}}">
-            <button>Add to basket</button> <br><br>
-</form>
+
+                <input type="hidden" name = "productID" value = "{{$item->productID}}">
+                <button>Add to basket</button> <br><br>
+            </form>
         </p> 
     </div>     
 </div>
