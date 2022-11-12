031f85ac1c2cad2e0bd130d5c01af60d12533ff7
Hasnain Ali <hasnain7815@gmail.com>
2022-11-12 20:27:28 +0000

    sorted some admin

--- Sarth-website/resources/views/layouts/admin.blade.php
+++ Sarth-website/resources/views/layouts/admin.blade.php
@@ -13,13 +13,13 @@ integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WT
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     
     </head>
-
     <div class="header">
         <a href="/admin" class="logo">Admin Dashboard</a>
         <div class="header-left">
             <a class="active" href="/admin">View all users</a>
             <a href="/admin/addNewGame">Add new game to products list</a>
             <a href="/admin/allProducts">View all products available</a>
+            <a href = "/logout">Logout</a>
         </div>        
     </div> 
 
