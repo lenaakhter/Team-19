01a90aa3a3827de177043ed81d22206a680801a3
Muniib-Ali <muniib11@live.com>
2022-11-14 13:28:19 +0000

    Fixed the issue where validation error messages did not come up

--- Sarth-website/app/Http/Kernel.php
+++ Sarth-website/app/Http/Kernel.php
@@ -19,10 +19,8 @@ class Kernel extends HttpKernel
         \Illuminate\Http\Middleware\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
-        \Illuminate\Session\Middleware\StartSession::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\UserAuth::class,
     ];
 
     /**
