7389c68b9d0be86450223351af1688e35c8323fb
lenaakhter <116122172+lenaakhter@users.noreply.github.com>
2022-11-20 14:28:37 +0000

    tidying up

--- Sarth-website/resources/views/layouts/main.blade.php
+++ Sarth-website/resources/views/layouts/main.blade.php
@@ -6,62 +6,65 @@ $sumOfItems= ProductsController::numOfItems() ?>
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Sarth</title>
-
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" 
-integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
         <link rel="stylesheet" href="/css/home.css">
-     
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-    
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+        <title>Sarth</title>
     </head>
 
-    <div class="header">
-        <a href="/" class="logo">Sarth</a>
-        <div class="header-left">
-            <a class="active" href="/">Home</a>
-            <a href="/products">Products</a>
-            <a href="/about">About</a>
-            <a href="/contact">Contact</a>
+    <div class= "bg-image">
+        <div class="header">
+            <a href="/" class="logo">Sarth</a>
+            <div class="header-left">
+                <a class="active" href="/">Home</a>
+                <a href="/products">Products</a>
+                <a href="/about">About</a>
+                <a href="/contact">Contact</a>
   
-        </div>
+            </div>
 
-        <!--             Basket for all Users                   -->
+            <!--             Basket for all Users                   -->
            @if(!(auth()->user()))  
-        <div class = "header-right">
-            <a href = "/userRegistration">Sign Up</a>
-            <a href="/login">Login</a>
-            <a href= "#" id="basket">Basket(0)</a>
+            <div class = "header-right">
+                <a href = "/userRegistration">Sign Up</a>
+                <a href="/login">Login</a>
+                <a href= "#" id="basket">Basket(0)</a>
             
-        </div>
-        @else
-        <div class = "header-right">
+            </div>
+            @else
+            <div class = "header-right">
 
-            <a href = "/orders">{{Session::get('user')['name']}}</a>
-            <a href = "/logout">Logout</a>
-            <a href="/basket">Basket({{$sumOfItems}})</a>
+                <a href = "/orders">{{Session::get('user')['name']}}</a>
+                <a href = "/logout">Logout</a>
+                <a href="/basket">Basket({{$sumOfItems}})</a>
             
-        </div>
+            </div>
 
-        @endif
+            @endif
 
-        <script type="text/javascript">
-    document.getElementById("basket").onclick = function () {
-        alert("you need to log in to view the basket");
-        location.href = "/login";
-    };
-</script>
+            <script type="text/javascript">
+                document.getElementById("basket").onclick = function () {
+                alert("you need to log in to view the basket");
+                location.href = "/login";
+                };
+            </script>
       
-    </div> 
+        </div> 
 
-    <body>
+        <body>
+            @yield('pageInfo')
+        </body>
 
-    @yield('pageInfo')
+        <footer>
+            <div class= "foot-bg">
+                <h2>Sarth</h2>
+                <a class="active" href="/">Home</a>
+                <a href="/products">Products</a>
+                <a href="/about">About</a>
+                <a href="/contact">Contact</a>
 
-    <footer>
-    </footer>
-
-    </body>
-
+            </div>
+        </footer>
+    
+    </div>
 </html>
--- Sarth-website/resources/views/userRegistration.blade.php
+++ Sarth-website/resources/views/userRegistration.blade.php
@@ -1,50 +1,43 @@
 @extends('layouts.main')
-<body>
 @section('pageInfo') 
 <br>
 <br>
 <div class = "Registration" id= "signup-page">
-<div class= "form-box">
-            <div class= "buttons-accounts">
-                <div id="btn"></div>
-                <button
-                type= "button" onclick=window.location.href="/login" class="toggle-btn" id="non-select"> Log In </button>
-                <button
-                type= "button" onclick=window.location.href="/userRegistration" class="toggle-btn" id="select">Sign Up</button>
-            </div>
-    <form action = "/userRegistration" method = "post" class="signin-inputs">
-        @csrf
-        <input type="text" name="Fullname" placeholder="Your name" class= "input-field2"/><br>
-        @error('Fullname')   
-        {{$message}}
-        <br>
-        @enderror
-        <input type="text" name="Username" placeholder = "Your Username" class="input-field2"/><br>
-        @error('Username')   
-        {{$message}}
-        <br>
-        @enderror
-        <input type = "email" name = "email" placeholder="Your email" class="input-field2"/><br>
-        @error('email')   
-        {{$message}}
-        <br>
-        @enderror
-	    <input type="password" name="password" placeholder = "Password" class="input-field2"/><br>
-        @error('password')   
-        {{$message}}
-        <br>
-        @enderror
-        <input type="password" name="password_confirmation" placeholder="Retype Password" class="input-field2"/><br>
-        @error('password_confirmation')   
-        {{$message}}
-        <br>
-        @enderror
-        <button type = "submit" class="reg-btn">Register</button>
-
-        
-    </form>
-</div>
+    <div class= "form-box">
+        <div class= "buttons-accounts">
+            <div id="btn"></div>
+            <button type= "button" onclick=window.location.href="/login" class="toggle-btn" id="non-select"> Log In </button>
+            <button type= "button" onclick=window.location.href="/userRegistration" class="toggle-btn" id="select">Sign Up</button>
+        </div>
+        <form action = "/userRegistration" method = "post" class="signin-inputs">
+            @csrf
+            <input type="text" name="Fullname" placeholder="Your name" class= "input-field2"/><br>
+            @error('Fullname')   
+            {{$message}}
+            <br>
+            @enderror
+            <input type="text" name="Username" placeholder = "Your Username" class="input-field2"/><br>
+            @error('Username')   
+            {{$message}}
+            <br>
+            @enderror
+            <input type = "email" name = "email" placeholder="Your email" class="input-field2"/><br>
+            @error('email')   
+            {{$message}}
+            <br>
+            @enderror
+	        <input type="password" name="password" placeholder = "Password" class="input-field2"/><br>
+            @error('password')   
+            {{$message}}
+            <br>
+            @enderror
+            <input type="password" name="password_confirmation" placeholder="Retype Password" class="input-field2"/><br>
+            @error('password_confirmation')   
+            {{$message}}
+            <br>
+            @enderror
+            <button type = "submit" class="reg-btn">Register</button>
+        </form>
+    </div>
 </div>
 @endsection
-</body>
-</html>
