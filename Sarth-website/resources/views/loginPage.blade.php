462e6ad862d3fe411118879ea302db63682725a4
lenaakhter <116122172+lenaakhter@users.noreply.github.com>
2022-11-22 19:09:13 +0000

    Update loginPage.blade.php

--- Sarth-website/resources/views/loginPage.blade.php
+++ Sarth-website/resources/views/loginPage.blade.php
@@ -1,54 +1,55 @@
-<div class="bg-image">
-    @extends('layouts.main')
-    @section('pageInfo')
+@extends('layouts.main')
+@section('pageInfo') 
 
-    @if(session()->has('BasketLoginMsg'))
-    <div class="alert alert-info" role="alert">
-        You need to Login to add to Basket
-    </div>
-    @endif
-    <br>
-    <br>
-
-    <!-- Login Form-->
-
-    <div class="login" id="login-page">
-        <div class="form-box">
-            <div class="buttons-accounts">
-                <div id="btn"></div>
-                <button type="button" onclick=window.location.href="/login" class="toggle-btn" id="select"> Log In
-                </button>
-                <button type="button" onclick=window.location.href="/userRegistration" class="toggle-btn"
-                    id="non-select">Sign Up</button>
-            </div>
-
-            <div class="errorMessage">
-                @if (session('status'))
-                {{session('status')}}
-                @endif
-            </div>
-            <form action="/login" method="post" class="login-inputs">
-                @csrf
-                <input type="email" name="email" placeholder="Your email" class="input-field" /><br>
-                <div class="errorlog">
-                    @error('email')
-                    {{ $message }}
-                    <br>
-                    @enderror
-                </div>
-                <input type="password" name="password" placeholder="Password" class="input-field" /><br>
-                <div class="errorlog">
-                    @error('password')
-                    {{ $message }}
-                    <br>
-                    @enderror
-                </div>
-                <input type="submit" value="Submit" class="submit-btn">
-            </form>
-        </div>
-        <br>
-        <br>
-
-    </div>
+@if(session()->has('BasketLoginMsg'))
+<div class="alert alert-info" role="alert">
+    You need to Login to add to Basket
 </div>
+@endif
+<br>
+<br>
+        
+<!-- Login Form-->
+
+<div class = "login" id="login-page">
+    <div class= "form-box1">
+        <div class= "buttons-accounts">
+            <div id="btn"></div>
+            <button type= "button" onclick=window.location.href="/login" class="toggle-btn" id="select"> Log In </button>
+            <button type= "button" onclick=window.location.href="/userRegistration" class="toggle-btn" id="non-select">Sign Up</button>
+        </div>
+
+        <div class = "error message">
+            @if (session('status'))
+            {{session('status')}}
+            @endif
+        </div>
+        <form action = "/login" method = "post" class="login-inputs">
+            @csrf 
+            <input type = "email" name = "email" placeholder="Your email" class="input-field"/><br>
+            <div class="errorlog">
+                @error('email')
+                {{ $message }}
+                <br>
+                @enderror
+            </div>
+            <input type="password" name="password" placeholder = "Password" class="input-field"/><br>
+            <div class="errorlog">
+                @error('password')   
+                {{ $message }}
+                <br>
+                @enderror
+            </div>
+            <br>
+            <input type = "submit" value ="Submit" class= "submit-btn">
+        </form>
+    </div>
+    <br>
+    <br>
+ 
+</div>
+
 @endsection
+
+
+</html>
