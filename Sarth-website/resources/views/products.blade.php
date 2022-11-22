651daae4e5322c458ad670f110903f49961a686f
pazara1208 <200148849@aston.ac.uk>
2022-11-22 22:42:38 +0000

    update

--- Sarth-website/resources/views/products.blade.php
+++ Sarth-website/resources/views/products.blade.php
@@ -2,7 +2,7 @@
 
 @section('pageInfo') 
 
-<head> 
+  <head> 
     <meta charset="utf-8">
     <meta name="view port" content="width=device-width, initial-scale-1">
     <title> Contact Us</title>
@@ -17,66 +17,72 @@
     <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@200;400;700&display=swap" rel="stylesheet">
 
     <script src="https://kit.fontawesome.com/c2e7142f29.js" crossorigin="anonymous"></script>
-</head>
-
-<div class=background>
+  </head>
   
-<!-- <style>  
-        body{
-        background-image: url('https://cdn.cloudflare.steamstatic.com/steam/apps/261570/ss_0063145dccd0cb165aa7d47d9ac6623445c7ad4c.1920x1080.jpg?t=1590605540');
-        background-size: cover;
-        }
-    </style> -->
-  <div class="title">
-      <h1>List of all games</h1>
-  </div>
+ 
+  <div class = "carousel-wrapper">
+    <span id="item-1"></span>
+    <span id="item-2"></span>
+    <span id="item-3"></span>
+    <div class="carousel-item item-1">
+      <a href="#item-3" class="arrow-prev arrow"></a>
+      <a href="#item-2" class="arrow-next arrow"></a>
+    </div>
 
-  <div class="container">
+    <div class="carousel-item item-2">
+      <a href="#item-1" class="arrow-prev arrow"></a>
+      <a href="#item-3" class="arrow-next arrow"></a>
+    </div>
 
-                      <div class="row height d-flex justify-content-center align-items-center">
-                        <div class="col-md-8">
-                          <div class="search">
-                            <form type="get" action= "{{ url('/search') }}">
-                              <input type="text" name="query" class="form-control" placeholder="Search for games">
-                              <button class="btn btn-primary" type= "submit">Search</button>
-                            </form>
-                          </div>
-                        </div>
-                      </div>
-                  </div>
-                  <br>
-</div>
+    <div class="carousel-item item-3">
+      <a href="#item-2" class="arrow-prev arrow"></a>
+      <a href="#item-1" class="arrow-next arrow"></a>
+    </div>
+  
+  
 
 
-<div class="gradient">  
-  <div class="games">
-      @foreach($products as $product)
-          <div class="game">
-              <a href="/products/{{ $product->productID }}">    
-                  <img src="{{ $product->imageLocation }} " alt="image of game" height="350px" width="330px"> 
-                  <div class="overlap">
-                    <div class="info">
-                      <p>
-                          {{ $product->productName }} <br><br>
-                          {{ $product->ageRating }} <br><br>
-                          £{{ $product->price }} <br><br>
-                      </p>
-                    </div>
-                  </div>
-              </a>  
+    <div class="container">
+
+      <div class="row height d-flex justify-content-center align-items-center">
+        <div class="col-md-8">
+          <div class="search">
+            <form type="get" action= "{{ url('/search') }}">
+              <input type="text" name="query" class="form-control" placeholder="Search for games">
+              <button class="btn btn-primary" type= "submit">Search</button>
+            </form>
           </div>
-      @endforeach
-  </div>
+        </div>
       </div>
+                    
+    </div>
+
+  </div>
+  
+
+  <div class="gradient"> 
+    <div class="title">
+      <h1>List of all games</h1>
+    </div> 
+    <div class="games">
+      @foreach($products as $product)
+        <div class="game">
+          <a href="/products/{{ $product->productID }}">    
+            <img src="{{ $product->imageLocation }} " alt="image of game" height="350px" width="330px"> 
+            <div class="overlap">
+              <div class="info">
+                <p>
+                    {{ $product->productName }} <br><br>
+                    {{ $product->ageRating }} <br><br>
+                    £{{ $product->price }} <br><br>
+                </p>
+              </div>
+            </div>
+          </a>  
+        </div>
+      @endforeach
+    </div>
+  </div>
 
 
 @endsection
-
-
-<!-- <style>
-.game{
-    border-style: solid;
-  border-color:white;
-  background-color: #E5FCE6 ;
-}
-    </style> -->
