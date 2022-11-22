d8be0147338209224ae712db1f2e8b62d29f1e82
lenaakhter <116122172+lenaakhter@users.noreply.github.com>
2022-11-22 12:08:25 +0000

    subtle changes

--- Sarth-website/resources/views/layouts/main.blade.php
+++ Sarth-website/resources/views/layouts/main.blade.php
@@ -16,6 +16,7 @@ $sumOfItems= ProductsController::numOfItems() ?>
         <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@200;400;700&display=swap" rel="stylesheet">
+    
 
     <title>Sarth</title>
 </head>
