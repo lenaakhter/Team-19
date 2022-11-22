ebc1e3d9da3c38cfcf3a14530a66130d70ac7da5
lenaakhter <116122172+lenaakhter@users.noreply.github.com>
2022-11-22 19:01:40 +0000

    Update loginPage.blade.php

--- Sarth-website/resources/views/loginPage.blade.php
+++ Sarth-website/resources/views/loginPage.blade.php
@@ -1,6 +1,6 @@
-<div class = "bg-image">
+<div class="bg-image">
     @extends('layouts.main')
-    @section('pageInfo') 
+    @section('pageInfo')
 
     @if(session()->has('BasketLoginMsg'))
     <div class="alert alert-info" role="alert">
@@ -9,42 +9,46 @@
     @endif
     <br>
     <br>
-            
+
     <!-- Login Form-->
 
-    <div class = "login" id="login-page">
-        <div class= "form-box">
-            <div class= "buttons-accounts">
+    <div class="login" id="login-page">
+        <div class="form-box">
+            <div class="buttons-accounts">
                 <div id="btn"></div>
-                <button type= "button" onclick=window.location.href="/login" class="toggle-btn" id="select"> Log In </button>
-                <button type= "button" onclick=window.location.href="/userRegistration" class="toggle-btn" id="non-select">Sign Up</button>
+                <button type="button" onclick=window.location.href="/login" class="toggle-btn" id="select"> Log In
+                </button>
+                <button type="button" onclick=window.location.href="/userRegistration" class="toggle-btn"
+                    id="non-select">Sign Up</button>
             </div>
 
-            <div class = "error message">
+            <div class="errorMessage">
                 @if (session('status'))
                 {{session('status')}}
                 @endif
             </div>
-            <form action = "/login" method = "post" class="login-inputs">
-                @csrf 
-                <input type = "email" name = "email" placeholder="Your email" class="input-field"/><br>
-                @error('email')
-                {{ $message }}
-                <br>
-                @enderror
-                <input type="password" name="password" placeholder = "Password" class="input-field"/><br>
-                @error('password')   
-                {{ $message }}
-                <br>
-                @enderror
-                <input type = "submit" value ="Submit" class= "submit-btn">
+            <form action="/login" method="post" class="login-inputs">
+                @csrf
+                <input type="email" name="email" placeholder="Your email" class="input-field" /><br>
+                <div class="errorlog">
+                    @error('email')
+                    {{ $message }}
+                    <br>
+                    @enderror
+                </div>
+                <input type="password" name="password" placeholder="Password" class="input-field" /><br>
+                <div class="errorlog">
+                    @error('password')
+                    {{ $message }}
+                    <br>
+                    @enderror
+                </div>
+                <input type="submit" value="Submit" class="submit-btn">
             </form>
         </div>
         <br>
         <br>
-    
+
     </div>
 </div>
 @endsection
-
-
