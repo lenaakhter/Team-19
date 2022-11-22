3f407c43824972e799424747a066387550b51561
lenaakhter <116122172+lenaakhter@users.noreply.github.com>
2022-11-22 20:23:24 +0000

    table changes

--- Sarth-website/resources/views/admin/adminPage.blade.php
+++ Sarth-website/resources/views/admin/adminPage.blade.php
@@ -1,34 +1,36 @@
 @extends('layouts.admin')
 
 @section('page')
+<div class="admin-table1">
+    <table>
+        <thead>
+            <tr>
+                <th>User ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Username</th>
+                <th>Are they a admin?</th>
+            </tr>
+        </thead>
 
-<div class="adminTable1">
-<table>
-    <thead>
-        <tr>
-            <th>User ID</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Username</th>
-            <th>Are they a admin?</th>
-        </tr>
-    </thead>
-    <body>
-        @foreach($users as $user)
-        <tr>
-            <td>{{ $user->id }}</td>
-            <td>{{ $user->name }}</td>
-            <td>{{ $user->email }}</td>
-            <td>{{ $user->username }}</td>
-            <td>
-                @if($user->isadmin)
-                Yes
-                @else
-                No
-                @endif
-            </td>
-        </tr>
-        @endforeach
-    </body>
-</table>
+        <body>
+            @foreach($users as $user)
+            <tr>
+                <td>{{ $user->id }}</td>
+                <td>{{ $user->name }}</td>
+                <td>{{ $user->email }}</td>
+                <td>{{ $user->username }}</td>
+                <td>
+                    @if($user->isadmin)
+                    Yes
+                    @else
+                    No
+                    @endif
+                </td>
+            </tr>
+            @endforeach
+        </body>
+    </table>
+</div>
+
 @endsection
