c07308251b499b431789df6af7c284b2afcfd25b
Sadiyahhh <116123629+Sadiyahhh@users.noreply.github.com>
2022-11-24 21:23:05 +0000

    updating checkout code to resolve conflicts

--- Sarth-website/resources/views/checkout.blade.php
+++ Sarth-website/resources/views/checkout.blade.php
@@ -16,48 +16,50 @@
       <div class="col-10">
 
         <div class="d-flex justify-content-between align-items-center mb-4">
-          <h3 class="fw-normal mb-0 text-white">Thanks for shopping with us {{Auth::user()->name}}, your Game Keys have been sent to {{Auth::user()->email}}</h3>
+          <h3 class="fw-normal mb-0 text-white">Thanks for shopping with us {{Auth::user()->name}}, your Game Keys have been sent to {{Auth::user()->email}}
+            Your Order No. is #{{$order->id}}
+          </h3>
         </div>
 
         <h1 style = "color: white;">Your Receipt: </h1><br>
-        @php $total = 0; @endphp
-        @foreach($order as $item)
+
+        @foreach($order->order_products as $product)
        
         <div class="card rounded-3 mb-4">
           <div class="card-body p-4">
             <div class="row d-flex justify-content-between align-items-center">
              <!-- Image of Product-->  
               <div class="col-md-2 col-lg-2 col-xl-2">
-                <img src="{{ $item->imageLocation}}" class="img-fluid rounded-3" alt="Game - img">
+                <img src="{{$product->productinfo->imageLocation}}"
+                 class="img-fluid rounded-3" alt="Game - img">
               </div>
               <!-- Name of Product-->
               <div class="col-md-3 col-lg-3 col-xl-3">
-                <p class="lead fw-normal mb-2"> {{$item->productName}}</p>
+                <p class="lead fw-normal mb-2"> {{$product->productinfo->productName}}</p>
               </div>
 
               <!-- Price of Product-->
               <div class="col-md-3 col-lg-2 col">
-                <h5 class="mb-0">£{{$item->price}}  </h5>
+                <h5 class="mb-0">£{{$product->productinfo->price}}  </h5>
               </div>
               
                 <!-- Quantity of Product-->
               <div class="col-md-3 col-lg-2 ">
-                <h5 class="mb-0">quantity: {{$item->qty}}  </h5>
+                <h5 class="mb-0">quantity: {{$product->qty}}  </h5>
               </div>
                <!-- Order no. and subtotal-->
                <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                <h5 class="mb-0">Order Status: <strong>{{$item->status}}</strong></h5>
+                <h5 class="mb-0">Order Status: <strong>{{$order->status}}</strong></h5>
               </div>
               
             </div>
           </div>
         </div>
-        @php $total+= $item->price * $item->qty;  @endphp
         @endforeach
         <div class="card">
           <div class="card-body">
-            <h2>Subtotal: £{{$total}} paid</h2>
-            <p>Ordered on = {{date('d-m-Y', strtotime($item->created_at))}}</p>
+            <h2>Subtotal: £{{$order->subtotal}} paid</h2>
+            <p>Ordered on = {{date('d-m-Y', strtotime($order->created_at))}}</p>
           </div>
         </div>
       </div>
