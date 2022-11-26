779a7348afcd7717f972841b7197700333049e69
lenaakhter <116122172+lenaakhter@users.noreply.github.com>
2022-11-26 15:29:54 +0000

    table styling for previous orders

--- Sarth-website/resources/views/orders.blade.php
+++ Sarth-website/resources/views/orders.blade.php
@@ -1,61 +1,55 @@
-@extends('layouts.main')
-<!-- JS Displays navbar using image -->
-<script>
-  document.addEventListener('DOMContentLoaded', (event) => {
-    let element = document.getElementById("header");
-    element.classList.add('bg-image');
-    element.style.backgroundColor = "black";
-    element.style.opacity = "0.95";
-})
-</script>
-@section('pageInfo')
+<div class="bg-image">
+    @extends('layouts.main')
+    @section('pageInfo')
 
-@if(count($orders) == 0)
-<div class="d-flex justify-content-between align-items-center mb-4">
-          <h3 class="fw-normal mb-0 text-black">You do not have any Previous orders</h3>
+    @if(count($orders) == 0)
+    <div class="no-orders">
+        <h3>You do not have any Previous orders</h3>
 
-        </div>
-@else
+    </div>
+    @else
+    <div class="orders-table-title">
+        <h3>Your Previous Order(s)</h3>
+    </div>
 
-          <h3 class="fw-normal mb-0 text-black">Your Previous Order(s)</h3>
+    <div class="orders-table">
+        <table>
+            <thead>
+                <tr>
+                    <th scope="col">Order ID</th>
+                    <th scope="col">Product ID</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">Subtotal</th>
+                    <th scope="col">Ordered on</th>
+                    <th scope="col">More Details</th>
+                </tr>
+            </thead>
+            @foreach($orders as $order)
 
-<table class="table table-striped">
-  <thead>
-    <tr>
-      <th scope="col">Order ID</th>
-      <th scope="col">Product ID</th>
-      <th scope="col">Email</th>
-      <th scope="col">Status</th>
-      <th scope="col">Subtotal</th>
-      <th scope="col">Ordered on</th>
-      <th scope="col">More Details</th>
+            <body>
 
+                <tr>
 
-
-    </tr>
-  </thead>
-  @foreach($orders as $order)
-  <tbody>
-
-    <tr>
-
-      <td>{{$order['id']}}</td>
-      <td>@foreach($order['order_products'] as $ord)
-        #{{$ord['productID']}}<br>
-        @endforeach
-      </td>
-      <td>{{$order['email']}}</td>
-      <td>{{$order['status']}}</td>
-      <td>£{{$order['subtotal']}}</td>
-      <td>{{date('d-m-Y', strtotime($order['created_at']))}}</td>
-      <td><a href="{{url('orders/'.$order['id'])}}">Order Details</a></td>
+                    <td>{{$order['id']}}</td>
+                    <td>@foreach($order['order_products'] as $ord)
+                        #{{$ord['productID']}}<br>
+                        @endforeach
+                    </td>
+                    <td>{{$order['email']}}</td>
+                    <td>{{$order['status']}}</td>
+                    <td>£{{$order['subtotal']}}</td>
+                    <td>{{date('d-m-Y', strtotime($order['created_at']))}}</td>
+                    <td><a href="{{url('orders/'.$order['id'])}}">Order Details</a></td>
 
 
-    </tr>
-    @endforeach
-  </tbody>
-</table>
-<i>Purchased Sarth keys are sent to the registered Email</i><br>
+                </tr>
+                @endforeach
+            </body>
+        </table>
+    </div>
+    <i>Purchased Sarth keys are sent to the registered Email</i><br>
 
     @endif
+</div>
 @endsection
