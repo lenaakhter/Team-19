37310a5678d33f05e8516ac472d42749a98278ed
lenaakhter <116122172+lenaakhter@users.noreply.github.com>
2022-11-27 14:54:04 +0000

    table on more details page

--- Sarth-website/resources/views/more-details.blade.php
+++ Sarth-website/resources/views/more-details.blade.php
@@ -1,38 +1,44 @@
-@extends('layouts.main')
+<div class="bg-image">
+    @extends ('layouts.main')
 
-@section('pageInfo')
+    @section('pageInfo')
 
 
-<div class="content">
-    <div class="title">
-        <h1>More Details</h1>
+    <div class="content">
+        <div class="title">
+            <h1>More Details</h1>
+        </div>
+
     </div>
 
-</div>
-<table class="table">
-  <thead>
+    <div class="orders-table">
+        <table class="table">
+            <thead>
 
-  <tr>
-      <th scope="col">Game</th>
-      <th scope="col">Title</th>
-      <th scope="col">Price</th>
-      <th scope="col">Quantity</th>
-      <th scope="col">Sarth Key</th>
-    </tr>
-  </thead>
-  <tbody>
-  @foreach($moreDetails->order_products as $product)
-    <tr>
-      <td> <img src="{{$product->productinfo->imageLocation}}"
-      class="img-fluid rounded-3" alt="Game - img" width="100" height="130"></td>
-      <td>{{$product->productinfo->productName }}</td>
-      <td>£{{$product->productinfo->price }}</td>
-      <td>{{$product->qty }}</td>
-      <td>{{$product->gamekey }}</td>
-    </tr>
-@endforeach
-  </tbody>
-</table>
-<br>
-<h3> Subtotal: £{{$moreDetails->subtotal}}</h3>
+                <tr>
+                    <th scope="col">Game</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Quantity</th>
+                    <th scope="col">Sarth Key</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($moreDetails->order_products as $product)
+                <tr>
+                    <td> <img src="{{$product->productinfo->imageLocation}}" class="img-fluid rounded-3"
+                            alt="Game - img" width="100" height="130"></td>
+                    <td>{{$product->productinfo->productName }}</td>
+                    <td>£{{$product->productinfo->price }}</td>
+                    <td>{{$product->qty }}</td>
+                    <td>{{$product->gamekey }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+        <h4> Subtotal: £{{$moreDetails->subtotal}}</h4>
+    </div>
+    <br>
+
+</div>
 @endsection
