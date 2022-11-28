07939a9dd28c52ed866337eaac4d396d4a877f73
lenaakhter <116122172+lenaakhter@users.noreply.github.com>
2022-11-28 21:45:18 +0000

    Revert "Revert "fixed orders info table""
    This reverts commit b427cf7d465ce5c7d855d06707bb235f96c1b2de.

--- Sarth-website/resources/views/admin/orders.blade.php
+++ Sarth-website/resources/views/admin/orders.blade.php
@@ -5,6 +5,7 @@
     @section('page')
 
     <div class="admin-tables">
+
         <table>
             <thead>
                 <tr>
@@ -13,12 +14,13 @@
                     <th>Email</th>
                     <th>Name</th>
                     <th>Product ID</th>
-                    <th>Product Name<th>
-                    <th>Product Quantity<th>
+                    <th>Product Name</th>
+                    <th>Product Quantity</th>
                     <th>Subtotal</th>
                     <th>Status of order</th>
                     <th>When order was created</th>
-                    <th>Order ID<th>
+                    <th>Order ID</th>
+
 
                 </tr>
             </thead>
@@ -28,30 +30,39 @@
                 <tr>
 
                     <td>{{ $order->userID }}</td>
+
                     <td>{{ $order->email }}</td>
+
                     <td>{{ $order->name }}</td>
+
                     <td>@foreach($order['order_products'] as $ord)
-                        #{{$ord['productID']}}<br><br>
+                        #{{$ord['productID']}}
                         @endforeach</td>
+
                     <td> @foreach($order->order_products as $product)
-                        {{$product->productinfo->productName}}<br><br>
-                        @endforeach<td>
-                        <td> @foreach($order->order_products as $product)
-                        x{{$product->qty}}<br><br>
-                        @endforeach<td>
+                        {{$product->productinfo->productName}}
+                        @endforeach</td>
+
+                    <td> @foreach($order->order_products as $product)
+                        x{{$product->qty}}
+                        @endforeach</td>
+
                     <td>£{{ $order->subtotal }}</td>
+
                     <td>
-                        <a href = "orders/{{ $order->id }}/{{ $order->status }}/{{ $order->userID }}">
-                        <button type =  "button">
-                        @if($order->status == "pending")
-                        pending
-                        @else
-                        completed
-                        @endif
-                        </button>
+                        <a href="orders/{{ $order->id }}/{{ $order->status }}/{{ $order->userID }}">
+                            <button type="button">
+                                @if($order->status == "pending")
+                                pending
+                                @else
+                                completed
+                                @endif
+                            </button>
                         </a>
                     </td>
+
                     <td>{{ $order->created_at }}</td>
+
                     <td>{{ $order->id }}</td>
                 </tr>
                 @endforeach
