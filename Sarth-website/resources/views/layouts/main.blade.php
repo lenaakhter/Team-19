a3210fead38565525016dd55ce3ce2788000285f
lenaakhter <116122172+lenaakhter@users.noreply.github.com>
2022-11-29 00:43:48 +0000

    main footer fixes (responsiveness added)

--- Sarth-website/resources/views/layouts/main.blade.php
+++ Sarth-website/resources/views/layouts/main.blade.php
@@ -18,6 +18,8 @@ $sumOfItems= ProductsController::numOfItems() ?>
         integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
     </script>
 
+    <script src="https://kit.fontawesome.com/c2e7142f29.js" crossorigin="anonymous"></script>
+
 
     <title>Sarth</title>
     <link rel="icon" type="image/x-icon" href="images/sarthicon.ico">
@@ -39,13 +41,14 @@ $sumOfItems= ProductsController::numOfItems() ?>
     <div class="header-right">
         <a href="/userRegistration">Sign Up</a>
         <a href="/login">Login</a>
-        <a href="#" id="basket">Basket(0)</a>
+        <a href="/basket" id="basket">Basket({{$sumOfItems}})</a>
 
     </div>
     @else
     <div class="header-right">
 
-        <a href="/orders">{{Session::get('user')['name']}}</a>
+        <a href="#">{{Session::get('user')['name']}}</a>
+        <a href="/orders">Your previous orders</a>
         <a href="/logout">Logout</a>
         <a href="/basket">Basket({{$sumOfItems}})</a>
 
@@ -54,13 +57,13 @@ $sumOfItems= ProductsController::numOfItems() ?>
     @endif
 
 
-    <!--script to redirect non logged-in user to Login page if they click basket -->
+   <!--
     <script type="text/javascript">
     document.getElementById("basket").onclick = function() {
         alert("you need to log in to view the basket");
         location.href = "/login";
     };
-    </script>
+    </script> -->
 
 </div>
 
@@ -73,15 +76,34 @@ $sumOfItems= ProductsController::numOfItems() ?>
         <br>
         <br>
         <div class="footer-content">
-            <div class="footer-section-about">
+            <div class="footer-section-about" id="foot-content-box">
                 <a href="/" class="logo"><img src="images/sarthgaming.png" alt="" /></a>
                 <p>Welcome to Sarth Gaming!</br>
-                    Here at Sarth we strive to deliver the latest </br>
-                    and trendiest games to our users. From young teens</br>
-                    to our older generations, there will be a game </br>
+                    Here at Sarth we strive to deliver the latest 
+                    and trendiest games to our users. From young teens
+                    to our older generations, there will be a game 
                     for you at Sarth Gaming Store. </p>
             </div>
-            <div class="footer-section-newsletter">
+            <div class="footer-section-links" id="foot-content-box">
+            @if(!(auth()->user()))
+                <a class="active" href="/">Home</a><br>
+                <a href="/products">Products</a><br>
+                <a href="/about">About</a><br>
+                <a href="/contact">Contact</a><br>
+                <br>
+                <a href="/userRegistration">Sign Up</a><br>
+                <a href="/login">Login</a><br>
+            @else
+                <a class="active" href="/">Home</a><br>
+                <a href="/products">Products</a><br>
+                <a href="/about">About</a><br>
+                <a href="/contact">Contact</a><br>
+                <br>
+                <a href="/basket">Basket({{$sumOfItems}})</a><br>
+                <a href="/logout">Logout</a>
+            @endif
+            </div>
+            <div class="footer-section-newsletter" id="foot-content-box">
                 <form action="">
                     <div class="footer-container">
                         <h3>Subscribe to our Newsletter</h3>
@@ -100,25 +122,6 @@ $sumOfItems= ProductsController::numOfItems() ?>
                         <input type="submit" value="Subscribe">
                     </div>
                 </form>
-            </div>
-            <div class="footer-section-links">
-            @if(!(auth()->user()))
-                <a class="active" href="/">Home</a><br>
-                <a href="/products">Products</a><br>
-                <a href="/about">About</a><br>
-                <a href="/contact">Contact</a><br>
-                <br>
-                <a href="/userRegistration">Sign Up</a><br>
-                <a href="/login">Login</a><br>
-            @else
-                <a class="active" href="/">Home</a><br>
-                <a href="/products">Products</a><br>
-                <a href="/about">About</a><br>
-                <a href="/contact">Contact</a><br>
-                <br>
-                <a href="/basket">Basket({{$sumOfItems}})</a><br>
-                <a href="/logout">Logout</a>
-            @endif
             </div>
         </div>
     </div>
