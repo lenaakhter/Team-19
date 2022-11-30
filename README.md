d825bcbafd9d88c1f10a23cc760e384ed2288db3
fahm781 <farazahmed9910@gmail.com>
2022-11-30 18:07:47 +0000

    The guest basket carries on if you register.

--- README.md
+++ README.md
@@ -11,3 +11,15 @@ Faraz Ahmed, Hussain Ahmed, Lena Akhter, Hasnain Ali, Muniib Ali, Sadiyah Begum,
 Description:
 
 Sarth is an e-commerce website that allows users to browse a wide catalog of video games. Users can purchase their desired video game(s) by adding them to their basket and checking out as a registered user. The games will be displayed with a brief description, cover picture, age rating, price, and will be categorised within genres such as Action, Mystery, Stealth etc. Our stakeholders will be people who enjoy playing video games as a hobby and our website will contain a wide range of games to reach as many target audiences as possible. The video game market typically adopts a more futuristic feel which we will display through varied colour schemes and layouts. Doing so provides us with a unique flair to appeal to our stakeholders. We have taken inspiration from websites such as Steam and Epic Games Launcher in the design of our website, though we plan to differ from these examples by making our website more user-friendly and attractive to the eye. Both these websites are quite monochromatic and confusing when you want to purchase a game. We will avoid this by utilising brighter, visually appealing colour schemes to keep users interested, as well as making sure that the website is easy to navigate.
+
+Requirements: 
+- PHP version: 8.1.10
+- Laravel Version: 9.39.0
+
+Step 1 - [Install XAMPP](https://www.apachefriends.org/download.html)  
+Step 2 - [Install Composer](https://getcomposer.org/download/) and follow the default installation steps.  
+Step 3 - If not cloned already, [Clone the Repo](https://github.com/lenaakhter/Team-19).  
+Step 4- Make sure to run the APACHE server and MySql from XAMPP  
+
+
+
--- Sarth-website/app/Http/Controllers/UserRegistrationController.php
+++ Sarth-website/app/Http/Controllers/UserRegistrationController.php
@@ -1,16 +1,19 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Basket;
 use App\Models\User;
 use App\Models\Userinformation;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Auth;
 
+use Illuminate\Support\Facades\DB;
 class UserRegistrationController extends Controller
 {
     /*
-    
+
     Displays the userRegistration view.
 
     */
@@ -20,18 +23,18 @@ class UserRegistrationController extends Controller
     }
 
     /*
-    
+
     Validates user information and creates a new user and logs them in.
 
      */
     public function storeUserInformation(Request $request){
         /*
-        
+
         Validates the data inputted by the user matches the set rules.
-        
+
         */
        $this->validate($request, [
-       
+
         'Fullname' => ['required', 'max:255'],
         'Username' => ['required', 'max:255'],
         'email' => ['required', 'max:255', 'unique:userinformation,email,exists,0'],
@@ -40,9 +43,9 @@ class UserRegistrationController extends Controller
        ]);
 
        /*
-       
+
        If validation is a new account is created.
-       
+
        */
        Userinformation::create([
         'username' => $request->Username,
@@ -55,7 +58,7 @@ class UserRegistrationController extends Controller
         'email' => $request-> email,
         'password' => $request->password
         ]);
-    
+
     /*
 
     User is logged in after account is created
@@ -64,7 +67,24 @@ class UserRegistrationController extends Controller
     $user= Userinformation::where(['email'=>$request->email])->first();
 
     $request->session()->put('user',$user);
+    if(session()->has('user')){
+        //if before logging in a guest session a basket exists, relate it to the user that is logging in
+          if (Basket::where('sessionID', Session::get('session_id'))->exists()){
+            $basketData= Basket::where('sessionID', Session::get('session_id'))->get();
+            foreach ($basketData as $id=>$basket){
 
+             $Basket = new Basket();
+             $Basket['userID'] = Auth::user()->id;
+             $Basket['email']= Auth::user()->email;
+             $Basket['productID'] = $basket['productID'];
+             $Basket['qty'] = $basket['qty'];
+             $Basket['price'] = $basket['price'];
+             $Basket->save();
+            }
+            //delete the old guest basket
+            DB::table('basket')->where('sessionID', Session::get('session_id'))->delete();
+        }
+            }
     return redirect('products');
     }
 }
