bc7b6bdbec950c7df1e0238ba9233ab77633d87b
Hasnain Ali <hasnain7815@gmail.com>
2022-12-01 19:33:45 +0000

    Fixed link for admin

--- Sarth-website/resources/views/admin/adminWelcome.blade.php
+++ Sarth-website/resources/views/admin/adminWelcome.blade.php
@@ -51,7 +51,7 @@
 
     <div class="admin-gif-2">
         <h2>Let's Get Started</h2>
-        <button type="button" onclick=window.location.href="/admin/users" class="redir">View Users</button>
+        <button type="button" onclick=window.location.href="/admin" class="redir">View Users</button>
     </div>
 
     <br>
